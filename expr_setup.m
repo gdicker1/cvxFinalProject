c7869f424d75c2266f8fb4d4b8e35e0b8a84215b
gdicker1 <gdicker1@uwyo.edu>
2019-05-12 20:32:12 -0600

    Add initial files for this project

---
+++ expr_setup.m
@@ -0,0 +1,8 @@
+function [Occupancies, UserHours, Fans, ACs, Computers] = expr_setup(numUsers, numRooms, numHours, numFans, numAC, numComputers)
+%EXPR_SETUP Given the number of users, fans, hours/day, fans, A/C units,
+%            and computers, output the appropriate Matrices/Vectors
+
+% Setup Occupancies matrix
+
+end
+
---
+++ main.m
@@ -0,0 +1,14 @@
+%{ 
+main.m
+Main file for cvxFinalProject
+
+Sets up experiement and calls other functions
+%}
+
+% Experiment defining variables
+numRooms = 10;
+numUsers = 10;
+numHours = 24;
+
+% Vectors / Matrices to define experiment
+
