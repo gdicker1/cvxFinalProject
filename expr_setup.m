0ec828462218e25bd37afddb9b4891d4d1c7a629
gdicker1 <gdicker1@github.com>
2019-05-13 21:53:17 -0600

    Finished expr_setup.m for initial testing

--- expr_setup.m
+++ expr_setup.m
@@ -3,11 +3,27 @@ function [Occupancies, UserHours, Fans, ACs, Computers] = expr_setup(numUsers, n
 %            and computers, output the appropriate Matrices/Vectors
 
 % Setup Occupancies matrix
+%  Each user only uses 1 room
+Occupancies = zeros(numRooms, numUsers);
+roomNums = randi([1 numRooms], numUsers, 1)
+for j = 1:numUsers
+   Occupancies(roomNums(j), j) = 1;
+end
 
 % Setup UserHours Matrix
-
+%  Randomly choose hours in a day that each user is in their lab
+UserHours = zeros(numHours, numUsers);
+for j = 1:numUsers
+    for i = 1:numHours
+       if rand() >= 0.5
+           UserHours(i,j) = 1;
+       else
+           UserHours(i,j) = 0;
+       end
+    end
+end
 % Setup Fans vectors
-Fans = 3*ones(numRooms, 1);
+Fans = ones(numRooms, 1);
 
 % Setup AC vector
 ACs = ones(numRooms, 1);
--- main.m
+++ main.m
@@ -4,20 +4,22 @@ Main file for cvxFinalProject
 
 Sets up experiement and calls other functions
 %}
+clear; clc;
 % seed rng
 rng('default');
 % Experiment defining variables
-numRooms = 10;
-numUsers = 10;
-numHours = 24;
-numFans = 3*numRooms;
-numAC = numRooms;
-numComp = 100;
+numUsers = int16(30);
+numRooms = int16(10);
+numHours = int16(24);
+numFans = int16(3*numRooms);
+numAC = int16(numRooms);
+numComp = int16(100);
 
 %% Power levels
 power_comp = 171; % Consumes 171Wh active (actual computer would have some percentage low power)
 power_fan = 200;
 power_ac = 400;
 
-% Vectors / Matrices to define experiment
+% Vectors & Matrices to define experiment
+[O, U, F, A, C] = expr_setup(numUsers, numRooms, numHours, numFans, numAC, numComp);
 
